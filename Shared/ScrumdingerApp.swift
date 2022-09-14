5945f2d29bdee2023e146a34c406852e54ed7867
ShamiTomita <shamitomita@gmail.com>
2022-09-14 09:43:23 -0400

    Initial Commit

---
+++ Shared/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Shared
+//
+//  Created by Shami Tomita on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
