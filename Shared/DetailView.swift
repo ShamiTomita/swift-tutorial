bd0357c9c00ecf74e73a479723ff8fb053a7b963
shamitomita <shamitomita@gmail.com>
2022-09-14 14:32:16 -0400

    adding detail view

---
+++ Shared/DetailView.swift
@@ -0,0 +1,23 @@
+//
+//  DetailView.swift
+//  Scrumdinger (iOS)
+//
+//  Created by Shami Tomita on 9/14/22.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView{
+        DetailView(scrum: DailyScrum.sampleData[0])
+        }
+    }
+}
