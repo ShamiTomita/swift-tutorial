584869b5cbc9a127e7013d224bec87aee95fef8c
shamitomita <shamitomita@gmail.com>
2022-09-15 21:56:28 -0400

    theme picker

--- Shared/Models/Theme.swift
+++ Shared/Models/Theme.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-enum Theme: String {
+enum Theme: String, CaseIterable, Identifiable {
     case bubblegum
     case buttercup
     case indigo
@@ -30,4 +30,7 @@ enum Theme: String {
     var name: String{
         rawValue.capitalized
     }
+    var id: String {
+            name
+        }
 }
